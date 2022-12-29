a9c0a4a2057b72e3f0e9977692973e2ec0605a02
surfer05 <rahul05alok@gmail.com>
2022-12-29 23:20:26 +0530

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header className="header"></Header>
+      {notes.map(createNotes)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
